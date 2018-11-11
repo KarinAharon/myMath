488ed21fda3b035f9f008fb46269d4a35b1ea581
KarinAharon <Krek250@gmail.com>
2018-11-11 11:26:17 +0200

    init repository with function

---
+++ function.java
@@ -0,0 +1,7 @@
+package myMath;
+
+/** This interface represents a simple function of type y=f(x), where both y and x are real numbers.
+**/
+public interface function {
+	public double f(double x);
+}
